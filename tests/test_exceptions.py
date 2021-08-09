12c000208bc9a04e313e581fa00ec346621cfd4d
Hju Kneyck Flores <hjucode@gmail.com>
2021-08-09 20:30:42 +0800

    test: increase test coverage

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,7 @@
+import pytest
+
 from pragmail import CommandError, IMAP4Error
+from pragmail.exceptions import _catch_exception
 
 
 class TestIMAP4Error:
@@ -20,3 +23,30 @@ class TestCommandError:
     def test_command_error(self):
         ce = CommandError("foo")
         assert "foo" in str(ce)
+
+
+def test_catch_exception_raises_IMAP4Error():
+    @_catch_exception
+    def raise_error():
+        raise Exception("error raised!")
+
+    with pytest.raises(IMAP4Error, match="error raised!"):
+        raise_error()
+
+
+def test_catch_exception_raises_CommandError_by_AttributeError():
+    @_catch_exception
+    def raise_error():
+        raise AttributeError
+
+    with pytest.raises(CommandError):
+        raise_error()
+
+
+def test_catch_exception_raises_CommandError_by_ValueError():
+    @_catch_exception
+    def raise_error():
+        raise ValueError
+
+    with pytest.raises(CommandError):
+        raise_error()
